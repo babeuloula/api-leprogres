86d1a0e36847906f5474a110b2a4cecfb52b4f52
Jérémy <babeuloula@gmail.com>
2020-10-01 08:51:09 +0200

    Install PHPCS & PHPStan (#2)

--- src/LeProgres/Api/Api.php
+++ src/LeProgres/Api/Api.php
@@ -1,93 +1,116 @@
 <?php
 
-    namespace LeProgres\Api;
+namespace LeProgres\Api;
 
-    use Exception;
+use Exception;
 
-    class Api {
+class Api
+{
 
-        private $base = "http://www.leprogres.fr/app_mobile/";
+    /** @var string */
+    private $base = "http://www.leprogres.fr/app_mobile/";
 
-        private $lastContents = "listcontentbytype?";
-        private $oneContent   = "detailwebcontent?";
+    /** @var string */
+    private $lastContents = "listcontentbytype?";
+    /** @var string */
+    private $oneContent = "detailwebcontent?";
 
-        private $appVersion        = ["appversion"          => "2.9.1"];
-        private $osVersion         = ["osversion"           => "7.0.1"];
-        private $appToken          = ["pTokenApplication"   => "8C841AA5-33C3-4325-80B1-C3A931579AF3"];
-        private $terminalMode      = ["pTerminalMode"       => 3];
-        private $userAuthenticated = ["isuserAuthenticated" => false];
+    /** @var string[] */
+    private $appVersion = ["appversion" => "2.9.1"];
+    /** @var string[] */
+    private $osVersion = ["osversion" => "7.0.1"];
+    /** @var string[] */
+    private $appToken = ["pTokenApplication" => "8C841AA5-33C3-4325-80B1-C3A931579AF3"];
+    /** @var int[] */
+    private $terminalMode = ["pTerminalMode" => 3];
+    /** @var false[] */
+    private $userAuthenticated = ["isuserAuthenticated" => false];
 
-        private $aviableContentType = [
-            "All",
-            "RichContent",
-            "Gallery",
-            "Video",
-            "Audio",
-            "Live",
-        ];
+    /** @var string[] */
+    private $aviableContentType = [
+        "All",
+        "RichContent",
+        "Gallery",
+        "Video",
+        "Audio",
+        "Live",
+    ];
 
-        private $url;
+    /** @var string */
+    private $url;
 
 
-        /**
-         * Récupère les derniers articles
-         *
-         * @param int    $page
-         * @param int    $per_page
-         * @param string $contentType
-         *
-         * @return array
-         * @throws Exception
-         */
-        public function getLastContents(int $page = 1, int $per_page = 20, String $contentType = "All") : array {
-            if(!in_array($contentType, $this->aviableContentType)) {
-                throw new Exception(sprintf("ContentType %s inconnu", $contentType), 400);
-            }
+    /**
+     * Récupère les derniers articles
+     *
+     * @param int $page
+     * @param int $per_page
+     * @param string $contentType
+     *
+     * @return mixed[]
+     * @throws Exception
+     */
+    public function getLastContents(int $page = 1, int $per_page = 20, string $contentType = "All"): array
+    {
+        if (false === \in_array($contentType, $this->aviableContentType)) {
+            throw new Exception(sprintf("ContentType %s inconnu", $contentType), 400);
+        }
 
-            $params = array_merge([
-                "page"        => $page,
-                "per_page"    => $per_page,
+        $params = array_merge(
+            [
+                "page" => $page,
+                "per_page" => $per_page,
                 "contenttype" => $contentType,
-            ], $this->appVersion, $this->osVersion, $this->appToken, $this->terminalMode, $this->userAuthenticated);
+            ],
+            $this->appVersion,
+            $this->osVersion,
+            $this->appToken,
+            $this->terminalMode,
+            $this->userAuthenticated
+        );
 
-            $params = http_build_query($params);
+        $params = http_build_query($params);
 
-            $this->url = $this->base . $this->lastContents . $params;
+        $this->url = $this->base . $this->lastContents . $params;
 
-            return Request::execute($this->url, "lastContents");
-        }
-
-
-        /**
-         * Récupère l'article en fonction de son URL
-         *
-         * @param string $cmsUrl
-         *
-         * @return array
-         * @throws Exception
-         */
-        public function getOneContent(String $cmsUrl) : array {
-            $params = array_merge([
-                "cmsurl" => $cmsUrl,
-            ], $this->appToken, $this->terminalMode, $this->userAuthenticated);
-
-            $params = http_build_query($params);
-
-            $this->url = $this->base . $this->oneContent . $params;
-
-            return Request::execute($this->url, "oneContent");
-        }
-
-
-
-
-
-        /**
-         * Permet de récupéré l'URL de l'API officielle qui est appelé
-         *
-         * @return String
-         */
-        public function getUrl() : String {
-            return $this->url;
-        }
+        return Request::execute($this->url, "lastContents");
     }
+
+
+    /**
+     * Récupère l'article en fonction de son URL
+     *
+     * @param string $cmsUrl
+     *
+     * @return mixed[]
+     * @throws Exception
+     */
+    public function getOneContent(string $cmsUrl): array
+    {
+        $params = array_merge(
+            [
+                "cmsurl" => $cmsUrl,
+            ],
+            $this->appToken,
+            $this->terminalMode,
+            $this->userAuthenticated
+        );
+
+        $params = http_build_query($params);
+
+        $this->url = $this->base . $this->oneContent . $params;
+
+        return Request::execute($this->url, "oneContent");
+    }
+
+
+    /**
+     * Permet de récupéré l'URL de l'API officielle qui est appelé
+     *
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+}
--- src/LeProgres/Api/Request.php
+++ src/LeProgres/Api/Request.php
@@ -1,52 +1,59 @@
 <?php
 
-    namespace LeProgres\Api;
+namespace LeProgres\Api;
 
-    use Exception;
-    use GuzzleHttp\Client;
-    use GuzzleHttp\Exception\GuzzleException;
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
-    class Request {
+class Request
+{
 
-        /**
-         * Permet d'exécuter les requêtes de l'API officielle du Progrès
-         *
-         * @param String $url  URL de l'API
-         * @param String $type Type de la requête
-         *
-         * @return array Données parsées correctement
-         * @throws Exception
-         */
-        public static function execute(String $url, String $type) : array {
-            $client = new Client();
+    /**
+     * Permet d'exécuter les requêtes de l'API officielle du Progrès
+     *
+     * @param String $url URL de l'API
+     * @param String $type Type de la requête
+     *
+     * @return mixed[] Données parsées correctement
+     * @throws Exception
+     */
+    public static function execute(string $url, string $type): array
+    {
+        $client = new Client();
 
-            try {
-                $res = $client->request('GET', $url);
-            } catch (GuzzleException | Exception $e) {
-                throw new Exception($e->getMessage(), 500);
-            }
-
-
-            $body = json_decode($res->getBody()->getContents());
-
-
-            if(json_last_error() !== JSON_ERROR_NONE) {
-                throw new Exception("Erreur lors de la conversion des contenus de l'API", 500);
-            }
-
-
-            if($res->getStatusCode() === 404 || $body->returnCode === 404) {
-                throw new Exception("Impossible de trouver le contenu dans l'API : " . $body->message, 404);
-            } else if($res->getStatusCode() != 200 || (!$body->success && $body->returnCode !== 0)) {
-                // Oui ne cherchez pas pourquoi, le code de retour de l'API n'est pas 200 mais 0 si la requête est bonne
-                throw new Exception("Erreur lors de la récupération des contenus dans l'API : " . $body->message, $res->getStatusCode());
-            }
-
-
-            $parser = new Parser();
-            $body = $parser->parse($body, $type);
-
-            return $body;
+        try {
+            $res = $client->request('GET', $url);
+        } catch (GuzzleException | Exception $e) {
+            throw new Exception($e->getMessage(), 500);
         }
 
+
+        $body = json_decode($res->getBody()->getContents());
+
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception("Erreur lors de la conversion des contenus de l'API", 500);
+        }
+
+
+        if ($res->getStatusCode() === 404 || $body->returnCode === 404) {
+            throw new Exception("Impossible de trouver le contenu dans l'API : " . $body->message, 404);
+        } elseif ((int) $res->getStatusCode() !== 200 || (false === $body->success && $body->returnCode !== 0)) {
+            // Oui ne cherchez pas pourquoi, le code de retour de l'API n'est pas 200 mais 0 si la requête est bonne
+            throw new Exception(
+                sprintf(
+                    "Erreur lors de la récupération des contenus dans l'API : %s",
+                    $body->message,
+                ),
+                $res->getStatusCode()
+            );
+        }
+
+
+        $parser = new Parser();
+        $body = $parser->parse($body, $type);
+
+        return $body;
     }
+}
